4e74bb5f772792c9431b3e0d21b0da73b2f66aac
Robert Kulyassa <qji@elte.hu>
2021-07-07 13:56:52 +0200

    yearly graphs added

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # google-ipv6-stats
 
-Is IPv6 usage growing exponential?
-
 This small python script displays the same data what you can see on Google IPv6 stat page but show the graph on exponential scale.
 
 Data source: https://www.google.com/intl/en/ipv6/statistics.html
--- google-ipv6-stats.py
+++ google-ipv6-stats.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 
+
 import urllib.request, urllib.error, urllib.parse
 import re
 import datetime
 import numpy
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import pandas as pd
 import csv
 
 response = urllib.request.urlopen('https://www.google.com/intl/en_ALL/ipv6/statistics/data/adoption.js')
 
 datalineregex=re.compile('^ *\[20.., .*\],$')
-timestamps=[]
-data=[]
-datamin=[]
-dataavg=[]
-datamax=[]
-last7=[ 0, 0, 0, 0, 0, 0 ,0]
-i=0
+timestamps = []
+data = []
+datamin = []
+dataavg = []
+datamax = []
+statdf = pd.DataFrame( columns = [ 'year', 'day', 'avg', 'min', 'max', 'norm_avg', 'norm_min', 'norm_max' ] )
+
+last7 = [ 0, 0, 0, 0, 0, 0 ,0]
+i = 0
+n = 0
+jan1 = {}
 with open('results/results.csv', 'w') as f:
     wr = csv.writer(f)
     for line in response.read().splitlines():
@@ -25,7 +31,8 @@ with open('results/results.csv', 'w') as f:
             trans = dict.fromkeys(map(ord, '[] '), None) # to remove '[', ']', ' '
             datalist = line.decode("utf-8").translate(trans).split(',')
             wr.writerow(datalist)
-            timestamps = timestamps+[ datetime.datetime(year=int(datalist[0]),month=int(datalist[1])+1,day=int(datalist[2])) ]
+            current_timestamp = datetime.datetime(year=int(datalist[0]),month=int(datalist[1])+1,day=int(datalist[2]))
+            timestamps = timestamps+[ current_timestamp ]
             last7[i]=float(datalist[3])
             if i == 6:
                 i=0
@@ -35,13 +42,38 @@ with open('results/results.csv', 'w') as f:
             datamin=datamin+[ min(last7) ]
             dataavg=dataavg+[ sum(last7)/7 ]
             datamax=datamax+[ max(last7) ]
-
-logbase=2
+            if current_timestamp.timetuple().tm_yday == 1:
+                # this is 1st January!
+                jan1[current_timestamp.year] = {
+                                                'avg': sum(last7)/7,
+                                                'min': min(last7),
+                                                'max': max(last7)
+                                               }
+            if current_timestamp.year in jan1.keys():
+                norm_avg = sum(last7)/7 - jan1[current_timestamp.year]['avg']
+                norm_min = min(last7)   - jan1[current_timestamp.year]['min']
+                norm_max = max(last7)   - jan1[current_timestamp.year]['max']
+            else:
+                norm_avg = None
+                norm_min = None
+                norm_max = None
+            statdf.loc[n] = [ int(current_timestamp.year), #year
+                              int(current_timestamp.timetuple().tm_yday), # day of the year
+                              sum(last7)/7, # avg
+                              min(last7),   # min
+                              max(last7),   # max
+                              norm_avg,
+                              norm_min,
+                              norm_max
+                            ]
+            #statdf.loc[n] = [ int(current_timestamp.year), int(current_timestamp.strftime('%j')), sum(last7)/7 ]
+            n += 1
 
 fig, ax = plt.subplots()
 ax.set_ylabel('log')
 #ax2 = ax.twinx()
 
+logbase=2
 timestamps_num=mdates.date2num(timestamps)
 print('first timestamp: '+str(timestamps[0])+'  ('+str(timestamps_num[0])+')')
 print('last timestamp:  '+str(timestamps[-1])+'  ('+str(timestamps_num[-1])+')')
@@ -59,5 +91,39 @@ ax.plot(timestamps_num, fit[1]+fit[0]*timestamps_num)
 
 plt.grid()
 plt.savefig('results/results.png')
+plt.show()
+
+#fig2, ax2 = plt.subplots()
+ax3 = plt.gca()
+# for year in range(2009, 2019):
+#     print(f" ------------------- { year } --------------------- ")
+#     print(statdf.loc[statdf['year'] == year ])
+# #    ax2.plot(x = statdf.loc[statdf['year'] == year ]['day'],
+# #             y = statdf.loc[statdf['year'] == year ]['percent'])
+#     statdf.loc[statdf['year'] == year ].plot(x = 'day', y = 'percent', ax = ax3)
+
+c = { '2013': 'red',
+      '2014': 'blue',
+      '2015': 'yellow',
+      '2016': 'purple',
+      '2017': 'green',
+      '2018': 'brown',
+      '2019': 'pink',
+      '2020': 'orange',
+      '2021': 'black',
+    }
+print(statdf)
+
+#statdf.loc[statdf['year'] > 2012 ].groupby('year').plot(x = 'day', y = 'norm_min', legend = False, ax = ax3)
+#statdf.loc[statdf['year'] > 2012 ].groupby('year').plot(x = 'day', y = 'norm_avg', legend = False, ax = ax3)
+statdf.loc[statdf['year'] > 2012 ].groupby('year').plot(x = 'day', y = 'norm_max', legend = False, ax = ax3)
+plt.title('IPv6 usage')
+plt.ylabel('%')
+plt.xlabel('Day of the year')
+current_handles, _ = plt.gca().get_legend_handles_labels()
+reversed_handles = reversed(current_handles)
+labels = reversed(statdf['year'].unique())
+plt.legend(reversed_handles,labels,loc='lower right')
+plt.grid()
 plt.show()
 
